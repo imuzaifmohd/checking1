b621eed63a7a1d19fc6af3fcfe1beef7a20bfcf3
Mohd Uzaif <imuzaifmohd@gmail.com>
2025-06-25 10:31:42 +0530

    Add project files.

---
+++ customerDemoWebApiPart2_withEntityFramework/Models/Admin.cs
@@ -0,0 +1,9 @@
+﻿namespace customerDemoWebApiPart2_withEntityFramework.Models
+{
+    public class Admin
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
